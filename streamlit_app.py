f3d2f3c94c602b06ce8a0376826db7e0e4b086aa
badacura <badacura@gmail.com>
2023-07-17 17:19:33 +0900

    2rpi

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,13 @@ st.write(msg)
 # 터미널(git bash, cmd) : pip install streamlit (ctrl + `)
 
 st.write(math.pi)
+
+# 원주율
+pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+# 반지름
+r = 15
+
+st.write('원주율 : ', pi)
+st.write('반지름 : ', r)
+st.write('원의 둘레 : ', 2 * pi * r)
+st.write('원의 넓이 : ', pi * r ** 2)
